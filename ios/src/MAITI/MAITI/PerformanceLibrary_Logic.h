083beca3026c207a08239bb0867ccc8cf63ffd9c
Roman A.Makhnenko <gvng@mail.ru>
2014-02-12 13:17:38 +0400

    Fix for multithreading problems

--- ios/src/MAITI/MAITI/PerformanceLibrary_Logic.h
+++ ios/src/MAITI/MAITI/PerformanceLibrary_Logic.h
@@ -11,29 +11,10 @@ MAITI is licensed under the terms and conditions of the MIT License as set forth
 
 #import <Foundation/Foundation.h>
 
-@interface PerformanceLibrary_Logic : NSObject{
-    NSDictionary *Preferences;          //user Preferences
-    
-    NSMutableDictionary *active_interval_transaction;   //For active interval transaction
-    NSMutableArray *completed_interval_transaction;    //For completed interval transaction
-    NSMutableDictionary *event_transaction;            //For Event
-    NSMutableArray *notification_transaction;          //For Notification transaction
-    
-    NSString *Session_Id;      // Session ID updates whenever the user logs into the app
-    NSString *customer_id;     // Customer ID set in the constructor
-    NSString *app_id;          // App ID set int he constructor
-    
-    NSString *app_code_ver;    // Version of the App defined in the bundle
-    NSString *package_id;      // Package ID defined in the bundle
-    Boolean Disabled;          // Whether the library is enabled or disabled
-    
-    NSTimer *TimeIntervalToConnectServer;
-    NSMutableArray *AllSendingData_Array;
-    
-    
-}
+@interface PerformanceLibrary_Logic : NSObject
 
-typedef enum {
+typedef enum
+{
     UserTag1,
     UserTag2,
     UserTag3,
@@ -44,22 +25,8 @@ typedef enum {
 } SupplementalTransactionArgs;
 
 
-@property(nonatomic,retain)NSDictionary *Preferences;
-
-@property(nonatomic,retain)NSMutableDictionary *active_interval_transaction;
-@property(nonatomic,retain)NSMutableArray *completed_interval_transaction;
-@property(nonatomic,retain)NSMutableDictionary *event_transaction;
-@property(nonatomic,retain)NSMutableArray *notification_transaction;
-
-
-@property(nonatomic,copy)NSString *Session_Id;
-@property(nonatomic,copy)NSString *customer_id;
-@property(nonatomic,copy)NSString *app_id;
-
-@property(nonatomic,copy)NSString *app_code_ver;
-@property(nonatomic,copy)NSString *package_id;
-@property(nonatomic)Boolean Disabled;
-
+@property(nonatomic,retain) NSDictionary *Preferences;
+@property(nonatomic) Boolean Disabled;
 
 -(id)initWithCustomerId:(NSString*) customerId appId:(NSString *)appId;
 -(NSString*)SetIntervalTransaction:(NSMutableDictionary*)parameters error:(NSError**)error;
